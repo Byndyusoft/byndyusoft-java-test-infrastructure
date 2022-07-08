76e1266a61194f6a92fc55585819f82982ecd55e
Averh <Dtyuhbzant!@#$56>
2022-07-08 13:01:02 +0500

    api client update

--- src/main/java/Byndyusoft/clients/RestApiBaseClient.java
+++ src/main/java/Byndyusoft/clients/RestApiBaseClient.java
@@ -1,49 +1,74 @@
 package Byndyusoft.clients;
 
 
-import com.google.gson.JsonObject;
-import io.restassured.http.ContentType;
-import io.restassured.response.Response;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 
-import java.io.File;
-
-import static io.restassured.RestAssured.given;
+import java.io.IOException;
 
 public class RestApiBaseClient {
 
-    public static Response getRequest(String headerName, String headerValue, String hostUrl) {
-        given()
-                .header(headerName, headerValue)
-                .when().get(hostUrl)
-                .then().log().status().log().body();
+    public static CloseableHttpResponse postWithParams(String uri, String contentType, HttpEntity entity)
+            throws IOException {
+        CloseableHttpClient client = HttpClients.custom().build();
+        HttpUriRequest request = RequestBuilder.post()
+                .setUri(uri)
+                .setHeader(HttpHeaders.ACCEPT, "*/*")
+                .setHeader(HttpHeaders.CONTENT_TYPE, contentType)
+                .setEntity(entity)
+                .build();
+        System.out.println(request);
 
-        return (Response) given().header(headerName, headerValue).when().get(hostUrl).then().log().status();
+        return client.execute(request);
+        /*String bodyAsString = EntityUtils.toString(response.getEntity());
+        int code = response.getStatusLine().getStatusCode();
+        client.close();
+
+        return bodyAsString + "\n" + "Статус ответа от сервера: " + code;*/
     }
 
-    public static Response postRequestWithJsonBody(String headerName, String headerValue, String hostUrl,
-                                                   JsonObject jsonBody) {
-        given()
-                .contentType(ContentType.JSON)
-                .body(jsonBody)
-                .header(headerName, headerValue)
-                .log().all()
-                .when().post(hostUrl)
-                .then().log().status().log().body();
+    public static CloseableHttpResponse postWithParamsA(String uri, String contentType, HttpEntity entity)
+            throws IOException {
+        CloseableHttpClient client = HttpClients.custom().build();
+        HttpUriRequest request = RequestBuilder.post()
+                .setUri(uri)
+                .setHeader(HttpHeaders.ACCEPT, "*/*")
+                .setHeader(HttpHeaders.CONTENT_TYPE, contentType)
+                .setHeader("id-token", "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijg4RkNCNEY2OEIyNEZEMDZDNkNFQTI1NTI0RTIyQ0MzMkM3ODQwNDYiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJpUHkwOW9za19RYkd6cUpWSk9Jc3d5eDRRRVkifQ.eyJuYmYiOjE2NTY0MTYwNzYsImV4cCI6MTY1NjQxNjM3NiwiaXNzIjoiaHR0cHM6Ly90LWxlZ29hdXRoLWFzMDIuaHEucnUuY29ycC5sZXJveW1lcmxpbi5jb20iLCJhdWQiOiJsbS1icG1zLXZlZC1leHQiLCJpYXQiOjE2NTY0MTYwNzYsImF0X2hhc2giOiJtMF9XODFoc0NkdnUyMjdLdWJ3U0Z3Iiwic3ViIjoiOTY5IiwiYXV0aF90aW1lIjoxNjU2NDEwMzE0LCJpZHAiOiJsb2NhbCIsInRpdGxlIjoidGVzdC5zYW1wbGVjb29yZGluYXRvciIsInZlZF9tb2RlIjoiVHJhZGUiLCJ2ZWRfb3JnYW5pemF0aW9uSWQiOiI2MGFjYjY3NmVjNjUxOTAwMTg4MGM1Y2QiLCJ2ZWRfcm9sZSI6IlNhbXBsZUNvb3JkaW5hdG9yIiwiZnVsbF9uYW1lIjoidGVzdC5zYW1wbGVjb29yZGluYXRvciIsIm5hbWUiOiJ0ZXN0LnNhbXBsZWNvb3JkaW5hdG9yIiwiZW1haWwiOiJ0ZXN0LnNhbXBsZWNvb3JkaW5hdG9yQGxlcm95bWVybGluLnJ1IiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImFtciI6WyJwd2QiXX0.Ps9a5knLQ1KELlzU7c5p66VEd_LPqTmp_OmkVtOdqtn0lyuojKjZooiCysdIMQ1mk73NkuiCmY-IMdeyBMEIK9HGaiOsNBRyFI_4BwKzZxGUs4P2z1XFm6BP1wCi6yFXCPgcrchGiQqffxyl44bm7FwkQRE9mvWxPhKg35KnGrxzzm5BG_BUuVLQ4d5WhPdk6iNHDDVTr0o_eVLnFLUDCltLNP3UkdThQzSpw_hh9BgxdopGmvaT-w3Y7osZ20EpgUDcf3VSRWtoXdR6skQ8qJEpax_HL-BDPtUaPEIV5ErOTcR9NOWIxT-pVbqdIfm_bq0Mfo6zVJi74z7minAXaA")
+                .setEntity(entity)
+                .build();
+        System.out.println(request);
 
-        return (Response) given().header(headerName, headerValue).when().get(hostUrl).then().log().status();
+        /*CloseableHttpResponse response = client.execute(request);
+        String bodyAsString = EntityUtils.toString(response.getEntity());
+        int code = response.getStatusLine().getStatusCode();
+        client.close();*/
+
+        return client.execute(request);
     }
 
-    public static Response postRequestWithFile(String headerName, String headerValue, String hostUrl, String filePath) {
-        given()
-                .contentType(ContentType.MULTIPART)
-                .multiPart("file", new File(filePath))
-                .body(filePath)
-                .header(headerName, headerValue)
-                .log().all()
-                .when().post(hostUrl)
-                .then().log().body().log().status();
+    public static CloseableHttpResponse getWithParams(String uri, Header[] headers)
+            throws IOException {
+        CloseableHttpClient client = HttpClients.custom().build();
+        HttpUriRequest request = RequestBuilder.get()
+                .setUri(uri)
+                .setHeader(HttpHeaders.ACCEPT, "*/*")
+                .build();
+        request.setHeaders(headers);
+        //System.out.println(headers);
+        //System.out.println(httpGet);
 
-        return given().header(headerName, headerValue).when().get(hostUrl).then().extract().response();
+        return client.execute(request);
+        /*int code = response.getStatusLine().getStatusCode();
+        client.close();
+
+        return Integer.toString(code);*/
     }
 
 }
--- src/tests/java/rest/test.java
+++ src/tests/java/rest/test.java
@@ -4,52 +4,26 @@ import Byndyusoft.clients.MongoDBBaseClient;
 import Byndyusoft.clients.PostgreSqlBaseClient;
 import Byndyusoft.clients.RabbitMQBaseClient;
 import Byndyusoft.clients.RestApiBaseClient;
-import Byndyusoft.configs.Property;
 import Byndyusoft.enums.SQLStatements;
-import com.google.gson.JsonObject;
-import io.restassured.response.Response;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.message.BasicHeader;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.concurrent.TimeoutException;
 
 
 public class test {
-
-
-    @Test
-    public void getTest() throws IOException {
-        String bearerToken = new Property().getProperty("bearerToken");
-        String headerName = "Authorization";
-        String headerValue = "Bearer "+ bearerToken;
-        String host = "https://gorest.co.in/public-api/users/";
-        String userId = "5965";
-        String hostUrl = host + userId;
-
-        RestApiBaseClient.getRequest(headerName,headerValue,hostUrl);
-
-    }
-
-    @Test
-    public void postTest() throws IOException {
-        String bearerToken = new Property().getProperty("bearerToken");
-        String headerName = "Authorization";
-        String headerValue = "Bearer "+ bearerToken;
-        String hostUrl = "https://gorest.co.in/public-api/users/";
-
-        JsonObject jsonBody = new JsonObject();
-
-        jsonBody.addProperty("name", "nameBlade");
-        jsonBody.addProperty("gender", "Male");
-        jsonBody.addProperty("email", "nameBlade@blade.com");
-        jsonBody.addProperty("status", "active");
-
-        RestApiBaseClient.postRequestWithJsonBody(headerName,headerValue,hostUrl,jsonBody);
-        //Assert.assertEquals();
-
-    }
 
     @Test
     public void mongoFindTest() throws IOException, InterruptedException {
@@ -66,17 +40,6 @@ public class test {
     }
 
     @Test
-    public void postWithFile() {
-        String hostUrl = "";
-        String headerName = "";
-        String headerValue = "";
-        String filePath= "";
-
-        Response response = RestApiBaseClient.postRequestWithFile(headerName,headerValue,hostUrl,filePath);
-        Assert.assertEquals(response, "200");
-    }
-
-    @Test
     public void rabbitPublish() throws IOException, TimeoutException {
         String hostName = "";
         Integer port = 0;
@@ -85,6 +48,68 @@ public class test {
         String queueName = "";
         String message = "";
         RabbitMQBaseClient.publishMessage(hostName, port, userName, userPassword, queueName, message);
+    }
+
+    @Test
+    public void getWithParams() throws IOException {
+        String uri = "https://backoffice-entity-repository-test-supchain-stage.apps.lmru.tech/emailNotificationGroups";
+        Header[] header = {
+                new BasicHeader("accept", "*/*")
+        };
+
+        CloseableHttpResponse response = RestApiBaseClient.getWithParams(uri, header);
+
+        System.out.println(response);
+
+        Assert.assertEquals(response.getStatusLine().getStatusCode(), 200);
+    }
+
+    @Test
+    public void getWithParamsAuth() throws IOException {
+        String uri = "https://backoffice-gui-test-supchain-stage.apps.lmru.tech/api/reports/cdf";
+        Header[] header = {
+                new BasicHeader("accept", "*/*"),
+                new BasicHeader("id-token", "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijg4RkNCNEY2OEIyNEZEMDZDNkNFQTI1NTI0RTIyQ0MzMkM3ODQwNDYiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJpUHkwOW9za19RYkd6cUpWSk9Jc3d5eDRRRVkifQ.eyJuYmYiOjE2NTY1MDIwMjQsImV4cCI6MTY1NjUwMjMyNCwiaXNzIjoiaHR0cHM6Ly90LWxlZ29hdXRoLWFzMDIuaHEucnUuY29ycC5sZXJveW1lcmxpbi5jb20iLCJhdWQiOiJsbS1icG1zLXZlZC1leHQiLCJpYXQiOjE2NTY1MDIwMjQsImF0X2hhc2giOiJnWVVUNEJBa3dyZ0YzUEtKRFA4MUtBIiwic3ViIjoiOTY3IiwiYXV0aF90aW1lIjoxNjU2NTAxNTcxLCJpZHAiOiJsb2NhbCIsInRpdGxlIjoiVHJhbnNwb3J0IEdyb3VwIFNwZWNpYWxpc3QiLCJ2ZWRfbW9kZSI6IlRyYWRlIiwidmVkX29yZ2FuaXphdGlvbklkIjoiNjA2MWRiNzQ1MDYxZTIzZTE0NTEwMjllIiwidmVkX3JvbGUiOiJUcmFuc3BvcnRHcm91cFNwZWNpYWxpc3QiLCJmdWxsX25hbWUiOiJ0ZXN0LnRyYW5zcG9ydGdyb3Vwc3BlY2lhbGlzdCIsIm5hbWUiOiJ0ZXN0LnRyYW5zcG9ydGdyb3Vwc3BlY2lhbGlzdCIsImVtYWlsIjoidGVzdC50cmFuc3BvcnRncm91cHNwZWNpYWxpc3RAbGVyb3ltZXJsaW4ucnUiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYW1yIjpbInB3ZCJdfQ.xsJpLoRveS7cSAvcG_37k51OlSEvb0O4iPNQIkVNCm0lIi_c66WoIX8-8x4OBtMtRD3UnGCrhdP414pWiWQSOnhoCf9HytVpezSMnW4d9A4IG7aGqseUK5vk2csuMXPNojqMZ_sKJXLxSqcVAUrgpXub1bhpZIpJgotBnRB2ED1jkiz1Ib8UlqunxMwDg1N5_bVNuIGXE8rO0om_CYa4MbkGjJt1ysVQEjxyPPSmSVHl-__Kf3COVH9mpetlgqxQe7DqFXykMkUQ59F8HJkk7uLpwjy-GSg0gMNH0kqlJTCz9QZ_zQTnrubM0c8Xj44Qv43J51bV6CWA3JbM-fCx5w")
+        };
+
+        CloseableHttpResponse response = RestApiBaseClient.getWithParams(uri, header);
+
+        InputStream inputStream =  response.getEntity().getContent();
+        String filePath = "src/main/resources/files/LI123.xlsx";
+        FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+        int inByte;
+        while((inByte = inputStream.read()) != -1)
+            fileOutputStream.write(inByte);
+        inputStream.close();
+        fileOutputStream.close();
+
+        System.out.println(response);
+
+        Assert.assertEquals(response.getStatusLine().getStatusCode(), 200);
+    }
+
+    @Test
+    public void postWithParams() throws IOException {
+        String json = "src/main/resources/json/body1.json";
+        StringEntity entity = new StringEntity(json);
+        CloseableHttpResponse code = RestApiBaseClient.postWithParamsA("https://backoffice-entity-repository-test-supchain-stage.apps.lmru.tech/orders/byOcNumbers",
+                "application/json",
+                entity);
+        System.out.println(code);
+    }
+
+    @Test
+    public void postWithParamsA() throws IOException {
+        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+        builder.addBinaryBody(
+                "file", new File("src/main/resources/files/LI.xlsx"), ContentType.APPLICATION_OCTET_STREAM, "LI.xlsx");
+
+        HttpEntity multipart = builder.build();
+
+        CloseableHttpResponse code = RestApiBaseClient.postWithParamsA("https://backoffice-gui-test-supchain-stage.apps.lmru.tech/api/reports/cdf",
+                "multipart/form-data",
+                multipart);
+        System.out.println(code);
     }
 
 }
